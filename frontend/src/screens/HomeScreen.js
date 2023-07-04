4e48994cd116fa0e0255fae58db35153d85279f6
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2023-07-04 13:57:14 +0530

    updated product .a map

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -56,11 +56,12 @@ function HomeScreen() {
             <MessageBox variant="danger">{error}</MessageBox>
           ) : (
             <Row>
-              {products.map((product) => (
-                <Col key={product._id} sm={6} md={4} lg={3} className="mb-3">
-                  <Product product={product} />
-                </Col>
-              ))}
+              {Array.isArray(products) &&
+                products.map((product) => (
+                  <Col key={product._id} sm={6} md={4} lg={3} className="mb-3">
+                    <Product product={product} />
+                  </Col>
+                ))}
             </Row>
           )}
         </div>
