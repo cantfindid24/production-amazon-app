bf16e22c44bdf49debb85648430760a19b0dfdf5
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2023-07-12 03:49:31 +0530

    added more banners and set img size

--- frontend/src/image-folder/banner/bannerImgsdata.js
+++ frontend/src/image-folder/banner/bannerImgsdata.js
@@ -3,6 +3,18 @@ import banner2 from '../banner/banner-2.jpg';
 import banner3 from '../banner/banner-3.jpg';
 import banner4 from '../banner/banner-4.jpg';
 import banner5 from '../banner/banner-5.jpg';
+import banner6 from '../banner/banner-6.jpg';
+import banner7 from '../banner/banner-7.jpg';
+import banner8 from '../banner/banner-8.jpg';
+import banner9 from '../banner/banner-9.jpg';
+import banner10 from '../banner/banner-10.jpg';
+import banner11 from '../banner/banner-11.jpg';
+import banner12 from '../banner/banner-12.jpg';
+import banner13 from '../banner/banner-13.jpg';
+import banner14 from '../banner/banner-14.jpg';
+import banner15 from '../banner/banner-15.jpg';
+import banner16 from '../banner/banner-16.jpg';
+import banner17 from '../banner/banner-17.jpg';
 
 const bannerimgs = [
   { src: banner1 },
@@ -10,6 +22,18 @@ const bannerimgs = [
   { src: banner3 },
   { src: banner4 },
   { src: banner5 },
+  { src: banner6 },
+  { src: banner7 },
+  { src: banner8 },
+  { src: banner9 },
+  { src: banner10 },
+  { src: banner11 },
+  { src: banner12 },
+  { src: banner13 },
+  { src: banner14 },
+  { src: banner15 },
+  { src: banner16 },
+  { src: banner17 },
 ];
 
 export default bannerimgs;
